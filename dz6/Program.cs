3e5f69c36d91e35672724d9571f065f9a7d64e5e
Dmitry1303 <45265651+Dmitry1303@users.noreply.github.com>
2018-12-12 13:53:27 +0300

    dz6

---
+++ dz6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz6
+{
+    class Program
+    {
+        /// <summary>
+        /// Акулов Д.С. домашняя #6
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Func.Start();
+            // запуск модуля с функциями
+            Console.ReadKey(); // не всегда использую свои методы, т.к. надо не забывать про базовые:-)
+        }
+
+    }
+}
